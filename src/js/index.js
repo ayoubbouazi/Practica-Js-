6956c704e4e083ed4b47101f9a3523dd017071a3
pol223 <poladrover7@gmail.com>
2021-11-25 19:34:26 +0100

    RegEx

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,49 @@ import { PantallaRegistro } from './registro';
 
 //Mostrar formulario registro usuario
 const pantallaRegistro = new PantallaRegistro(document.querySelector('#registro'));
+            //RegEx de formulario(validacion)
+            const input = document.querySelectorAll('input');
 
+            Array.from(input);
+            const expresiones = {
+                name: /^[A-Za-z ]+$/,
+                email: /^\w+([\.\+\-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/,
+                password: /^[A-Za-z0-9]{5}$/,
+            };
+            
+            const validacion = (expresion, inp, id) => {
+                if (expresion.test(inp.value)) {
+                    document.getElementById(`${id}`).classList.remove('formularioIncorrecto');
+                    document.getElementById(`${id}`).classList.add('formularioCorrecto');
+                } else {
+                    document.getElementById(`${id}`).classList.add('formularioIncorrecto');
+                    document.getElementById(`${id}`).classList.remove('formularioCorrecto');
+                }
+            };
+            
+            const inputs = document.querySelectorAll('.input');
+            
+            const validarFormulario = (e) => {
+                switch (e.target.name)
+                {
+                case 'nombre':
+                    validacion(expresiones.name, e.target, 1);
+                    break;
+                case 'email':
+                    validacion(expresiones.email, e.target, 2);
+                    break;
+                case 'password':
+                    validacion(expresiones.password, e.target, 3);
+                    break;
+                default:
+                    break;
+                }
+            };
+            
+            inputs.forEach((e) => {
+                e.addEventListener('keyup', validarFormulario);
+                e.addEventListener('blur', validarFormulario);
+            });
 
 //Una vez registrado, mostrar la pantalla de equipos
 
--- src/js/registro/index.js
+++ src/js/registro/index.js
@@ -11,49 +11,6 @@ export class PantallaRegistro {
 
             //Recuperar campos formulario
 
-            //RegEx de formulario(validacion)
-            const expresiones = {
-                name: /^[A-Za-z ]+$/,
-                email: /^\w+([\.\+\-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/,
-                password: /^[A-Za-z0-9]{5}$/,
-            };
-
-            const validacion = (expresion, inp, id) => {
-                if (expresion.test(inp.value)) {
-                    document.getElementById(`${id}`).classList.remove('formularioIncorrecto');
-                    document.getElementById(`${id}`).classList.add('formularioCorrecto');
-                } else {
-                    document.getElementById(`${id}`).classList.add('formularioIncorrecto');
-                    document.getElementById(`${id}`).classList.remove('formularioCorrecto');
-                }
-            };
-
-            const inputs = document.querySelectorAll('.input');
-
-            const validarFormulario = (e) => {
-                switch (e.target.name)
-                {
-                case 'nombre':
-                    validacion(expresiones.name, e.target, 1);
-                    break;
-                case 'email':
-                    validacion(expresiones.email, e.target, 2);
-                    break;
-                case 'password':
-                    validacion(expresiones.password, e.target, 3);
-                    break;
-                default:
-                    break;
-                }
-            };
-
-            inputs.forEach((e) => {
-                e.addEventListener('keyup', validarFormulario);
-                e.addEventListener('blur', validarFormulario);
-            });
-
-            
-
             //Enviar formulario usando fetch a https://jsonplaceholder.typicode.com
 
             //Devolver usuario creado
